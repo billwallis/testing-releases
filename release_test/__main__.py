5d54a92d8ad18143947c29226034cf9cb150e2d0
Bill <Bill@TheWallisFamily.co.uk>
2023-08-04 17:15:53 +0100

    ✨ feat: Create sample script with a CLI command

---
+++ release_test/__main__.py
@@ -0,0 +1,14 @@
+"""
+Sample file for testing the release process.
+"""
+
+
+def main() -> None:
+    """
+    Run the test scripts.
+    """
+    print("Qapla'!")
+
+
+if __name__ == "__main__":
+    main()
