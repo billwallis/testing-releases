9acbbb07654b7e1a79e318589987db22f1eb1ec9
Bill <bill@thewallisfamily.co.uk>
2023-11-04 16:52:41 +0000

    fix: Add an extra success message

--- testing_releases/__main__.py
+++ testing_releases/__main__.py
@@ -8,6 +8,7 @@ def main() -> None:
     Run the test scripts to confirm that the release works.
     """
     print("Qapla'! This is Klingon for 'success'!")
+    print("Bingo! The thing went well!")
 
 
 if __name__ == "__main__":
